d8f776e30d6bf22cc5d8f40cb36b1221a3fd0c4a
kishorekarunakaran <kishorekarunakaran@kishorekarunakarans-MacBook-Pro.local>
2021-12-13 23:00:58 +0530

    Changes with respect to class function

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -18,20 +18,20 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>{this.state.current}</p>
-          <button onClick={() => this.setState(changeText(this.state))}>
+          <button onClick={() => this.setState(this.changeText(this.state))}>
             Change Text
           </button>
         </header>
       </div>
     );
   }
-}
 
-function changeText(state) {
-  let temp = state.current;
-  state.current = state.previous;
-  state.previous = temp;
-  return state;
+  changeText(state) {
+    let temp = state.current;
+    state.current = state.previous;
+    state.previous = temp;
+    return state;
+  }
 }
 
 export default App;
