6d735789be1583e96f012efa9510796df31ec748
khekrn <Life@2020withJava>
2022-04-01 09:51:35 +0530

    Changes in lesson 2, Getting closer to the completion

--- lesson2/monsters-rolodex/src/App.js
+++ lesson2/monsters-rolodex/src/App.js
@@ -7,19 +7,42 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchString: "",
     };
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({monsters: users}));
+      .then((users) =>
+        this.setState({ monsters: users, currentMonsters: users })
+      );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState(() => {
+      return {
+        searchString: searchField,
+      };
+    });
+  };
+
   render() {
+    const { monsters, searchString } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonster = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchString);
+    });
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monster"
+          onChange={onSearchChange}
+        ></input>
+        {filteredMonster.map((monster) => (
           <h1 key={monster.id}> {monster.name} </h1>
         ))}
       </div>
